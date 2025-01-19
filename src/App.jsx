b910dbcebf7ef9806d27f40ecfbb1360733f928d
Fereshteakbari <fereshteakbariam18@gmail.com>
2025-01-19 12:58:22 +0330

    up to date codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import ProductsPage from "./pages/ProductsPage";
+import DetailsPage from "./pages/DetailsPage";
+import CheckoutPage from "./pages/CheckoutPage";
+import PageNotFound from "./pages/404";
+import ProductsProvider from "./context/ProductContext";
 function App() {
-  return <></>;
+  return (
+    <ProductsProvider>
+      <Routes>
+        <Route index element={<Navigate to="/products" replace />} />
+        <Route path="/products" element={<ProductsPage />} />
+        <Route path="/products/:id" element={<DetailsPage />} />
+        <Route path="checkout" element={<CheckoutPage />} />
+        <Route path="/*" element={<PageNotFound />} />
+      </Routes>
+    </ProductsProvider>
+  );
 }
 
 export default App;
---
+++ src/pages/ProductsPage.jsx
@@ -0,0 +1,81 @@
+import { ImSearch } from "react-icons/im";
+import Card from "../components/Card";
+import { useProducts } from "../context/ProductContext";
+import styles from "./ProductsPage.module.css";
+import Loader from "../components/Loader";
+import { useEffect, useState } from "react";
+import { FaListUl } from "react-icons/fa";
+import {
+  createQueryObject,
+  filterProducts,
+  getInitialQuery,
+  searchProducts,
+} from "../helpers/helper";
+import { useSearchParams } from "react-router-dom";
+function ProductsPage() {
+  const products = useProducts();
+  const [displayed, setDisplayed] = useState([]);
+  const [search, setSearch] = useState();
+  const [query, setQuery] = useState({});
+  const [searchParams, setSearchParams] = useSearchParams();
+  useEffect(() => {
+    setDisplayed(products);
+    setQuery(getInitialQuery(searchParams));
+  }, [products]);
+
+  useEffect(() => {
+    setSearchParams(query);
+    setSearch(query.search || " ");
+    let finalProducts = searchProducts(products, query.search);
+    finalProducts = filterProducts(finalProducts, query.category);
+    setDisplayed(finalProducts);
+  }, [query]);
+
+  const searchHandler = () => {
+    setQuery((query) => createQueryObject(query, { search }));
+  };
+  const categoryHandler = (event) => {
+    const { tagName } = event.target;
+    const category = event.target.innerText.toLowerCase();
+    if (tagName !== "LI") return;
+    setQuery((query) => createQueryObject(query, { category }));
+  };
+  return (
+    <>
+      <div>
+        <input
+          type="text"
+          placeholder="Search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value.toLowerCase().trim())}
+        />
+        <button onClick={searchHandler}>
+          <ImSearch />
+        </button>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {!displayed.length && <Loader />}
+          {displayed.map((p) => (
+            <Card key={p.id} data={p} />
+          ))}
+        </div>
+        <div>
+          <div>
+            <FaListUl />
+            <p>Categories</p>
+          </div>
+          <ul onClick={categoryHandler}>
+            <li>All</li>
+            <li>Electronic</li>
+            <li>Jewelery</li>
+            <li>Men's Clothing</li>
+            <li>Womens' Clothing</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductsPage;
